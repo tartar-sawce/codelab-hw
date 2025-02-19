54c560c7c0d82e750053bf5dfc741720a8ca5365
amna <amnairfantarar@gmail.com>
2025-02-18 22:31:34 -0500

    much ado hw

---
+++ w9-much_ado.py
@@ -0,0 +1,24 @@
+with open('much_ado.txt', 'r') as infile:
+    play = infile.read()
+
+# create variables to which we will add line and character counts.
+Beatrice_lines = 0
+Beatrice_length = 0
+Benedick_lines = 0
+Benedick_length = 0
+
+for line in play.split("\n\n"):
+    if line.startswith("BEATRICE"):
+        Beatrice_lines += 1
+        # ".strip" removes white space at the beginning and end of a string.
+        # "-9" removes "BEATRICE./ BENEDICK." from the character count, so the character name isn't counted.
+        Beatrice_length += len(line.strip())-9
+
+    elif line.startswith("BENEDICK"):
+        Benedick_lines += 1
+        Benedick_length += len(line.strip())-9
+
+print("Beatrice has " + str(Beatrice_lines) + " lines comprising " + str(Beatrice_length) + " characters, while Benedick has " + str(Benedick_lines) + " lines comprising " + str(Benedick_length) + " characters.")
+        
+
+
